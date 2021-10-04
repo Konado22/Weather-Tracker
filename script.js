c4090e37e9d9bb5075c104e3dc8f7a2689b56734
Jesse Draper <jessedraper2@gmail.com>
2021-10-04 13:03:10 -0600

    further update html and add js

---
+++ script.js
@@ -0,0 +1,7 @@
+var searchBtn = document.getElementById("searchBtn")
+var cityName = 
+var content = fetch(api.openweathermap.org/data/2.5/weather?q={}&appid={dd896623669fb986e67b89582e431e10}){
+    city:( )
+}
+
+ var callCity = searchBtn.addEventListener("click",)
