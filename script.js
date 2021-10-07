58dbbb1d05a7ca2fb42760b60f7f9290c319b00d
Jesse Draper <jessedraper2@gmail.com>
2021-10-06 18:18:22 -0600

    update js and html structure

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+console.log("hello")
 var searchBtn = document.getElementById("searchBtn")
-var cityName = 
-var content = fetch(api.openweathermap.org/data/2.5/weather?q={}&appid={dd896623669fb986e67b89582e431e10}){
-    city:( )
-}
+var RememberMe = document.querySelector("ul")
+var tempSpace = document.getElementById("temp")
+var windSpace = document.getElementById("wind")
+var humidSpace = document.getElementById("humidity")
+var uvSpace = document.getElementById("uv")
 
- var callCity = searchBtn.addEventListener("click",)
+
+
+
+function callCity (){
+    var constMe = document.getElementById("exampleFormControlTextarea1");
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q="+constMe.value+"&appid=dd896623669fb986e67b89582e431e10";
+    fetch(requestUrl)
+    .then(response => response.json())
+    .then( data => {
+        console.log(data)
+    tempSpace.innerText = data['main']['temp']
+    windSpace.innerText = data.wind.speed
+    humidSpace.innerText = data.main.humidity 
+    })
+.catch( err => {
+    console.log(err)
+
+})};
+searchBtn.addEventListener('click', callCity);
